2285c4e5d027ed7b95661edfbf77f77cfcbbb736
Brett Grentell <doublewishgrentell@gmail.com>
2022-08-11 19:35:47 +1000

    Uncached some files from git

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "381242051737-u41fp065l1mqp5bfdmfbaqbu6uf9e4a6.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI:
-    "mongodb+srv://doublewish:1_wHYPXgpE@cluster0.jo5vk.mongodb.net/emailydb?retryWrites=true&w=majority",
-  cookieKey: "cughtnglsifhjcyfiqpcdnijkaovfhdvjlfhdsdfchsldjk",
-};
